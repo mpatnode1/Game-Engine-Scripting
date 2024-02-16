bd90089ec55410341a827cc63d6b2b92b00fdc37
mpatnode1 <149105967+mpatnode1@users.noreply.github.com>
2024-02-16 16:31:07 -0600

    setup ui
    - added grid
    - added buttons + code for movement
    - need to finish firing

--- GES-Project/Assets/Week-3/Scripts/GameManager.cs
+++ GES-Project/Assets/Week-3/Scripts/GameManager.cs
@@ -1,18 +1,127 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class GameManager : MonoBehaviour
+namespace Battleship
 {
-    // Start is called before the first frame update
-    void Start()
+    public class GameManager : MonoBehaviour
     {
-        
-    }
+        [SerializeField]
+        private int[,] grid = new int[,]
+        {
+            //The top left is (0,0)
+            {1, 1, 0, 0, 1},
+            {0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 1},
+            {1, 0, 1, 0, 0},
+            {1, 0, 1, 0, 1}
+            //Bottom right is 5, 5
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        };
+
+        //Represents where the player has fired
+        private bool[,] hits;
+
+        //Total rows and columns we have
+        private int nRows;
+        private int nCols;
+
+        //Current row/column we are on
+        private int row;
+        private int col;
+
+        //Correctly hit ships
+        private int score;
+
+        //Total time game has been running
+        private int time;
+
+        //Parent of all cells
+        [SerializeField] Transform gridRoot;
+
+        //Template used to populate the grid
+        [SerializeField] GameObject cellPrefab;
+        [SerializeField] GameObject winLabel;
+        [SerializeField] TextMeshProUGUI timeLabel;
+        [SerializeField] TextMeshProUGUI scoreLabel;
+
+        private void Awake()
+        {
+            //Initialize rows/cols to help us with our operations
+            nRows = grid.GetLength(0);
+            nCols = grid.GetLength(1);
+            //Create identical 2D array to grid that is of the type bool instead of int
+            hits = new bool[nRows, nCols];
+
+            //Populate the grid using a loop
+            //Needs to execute as many times to fill up the grid
+            //Can figure this out by calculating rows + cols
+            for (int i = 0; i < nRows * nCols; i++)
+            {
+                //Create an instance of the prefab and child it to the gridRoot
+                Instantiate(cellPrefab, gridRoot);
+            }
+
+            SelectCurrentCell();
+        }
+
+        Transform GetCurrentCell()
+        {
+            //You can figure out the child index
+            //of the cell that is a part of the grid
+            //by calculating (rows*Cols) + col
+            int index = (row * nCols) + col;
+            //Return the child by index
+            return gridRoot.GetChild(index);
+        }
+
+        void SelectCurrentCell()
+        {
+            //Get the current cell
+            Transform cell = GetCurrentCell();
+            //Set the "Cursor" image on
+            Transform cursor = cell.Find("Cursor");
+            cursor.gameObject.SetActive(true);
+        }
+
+        void UnselectCurrentCell()
+        {
+            //Get the current cell
+            Transform cell = GetCurrentCell();
+            //Set the "Cursor" image off
+            Transform cursor = cell.Find("Cursor");
+            cursor.gameObject.SetActive(false);
+        }
+
+        public void MoveHorizontal(int amt)
+        {
+            //Since we are moving to a new cell
+            //we need to unselect the previous one
+            UnselectCurrentCell();
+
+            //Update the column
+            col += amt;
+            //Make sure the column stays within the bounds of the grid
+            col = Mathf.Clamp(col, 0, nCols - 1);
+
+            //Select the new cell
+            SelectCurrentCell();
+        }
+
+        public void MoveVertical(int amt)
+        {
+            //Since we are moving to a new cell
+            //we need to unselect the previous one
+            UnselectCurrentCell();
+
+            //Update the column
+            row += amt;
+            //Make sure the column stays within the bounds of the grid
+            row = Mathf.Clamp(row, 0, nRows - 1);
+
+            //Select the new cell
+            SelectCurrentCell();
+        }
     }
 }
