e0ef80ab97b2b84933c0ff42d26d4414419bb9d2
mpatnode1 <149105967+mpatnode1@users.noreply.github.com>
2024-02-16 18:25:33 -0600

    added fire button
    - added code for fire button
    - added code for timer
    - stylized button images

--- GES-Project/Assets/Week-3/Scripts/GameManager.cs
+++ GES-Project/Assets/Week-3/Scripts/GameManager.cs
@@ -64,6 +64,7 @@ namespace Battleship
             }
 
             SelectCurrentCell();
+            InvokeRepeating("IncrementTime", 1f, 1f);
         }
 
         Transform GetCurrentCell()
@@ -122,6 +123,93 @@ namespace Battleship
 
             //Select the new cell
             SelectCurrentCell();
+        }
+
+        void ShowHit()
+        {
+            //Get the current cell
+            Transform cell = GetCurrentCell();
+            //Set the "Hit" image on
+            Transform hit = cell.Find("Hit");
+            hit.gameObject.SetActive(true);
+        }
+
+        void ShowMiss()
+        {
+            //Get the current cell
+            Transform cell = GetCurrentCell();
+            //Set the "Miss" image on
+            Transform miss = cell.Find("Miss");
+            miss.gameObject.SetActive(true);
+        }
+
+        void IncrementScore()
+        {
+            //Add 1 to the score
+            score++;
+            //Update the score label with current score
+            scoreLabel.text = string.Format("Score: {0}", score);
+        }
+
+        public void Fire()
+        {
+            //Checks if the cell in the hits data is true or false
+            //If it's true that means we already fired a shot in the current cell
+            //And we should not do anything
+            if (hits[row, col]) return;
+
+            //Mark this cell as being fired upon
+            hits[row, col] = true;
+
+            //If this cell is a ship
+            if (grid[row, col] == 1)
+            {
+                //Hit it
+                //Increment score
+                ShowHit();
+                IncrementScore();
+            }
+            //If the cell is open water
+            else
+            {
+                //Show miss
+                ShowMiss();
+            }
+
+            TryEndGame();
+        }
+
+        void TryEndGame()
+        {
+            //Check every row
+            for (int row = 0; row < nRows; row++)
+            {
+                //And check every column
+                for (int col = 0; col < nCols; col++)
+                {
+                    //If a cell is not a ship then we can ignore
+                    if (grid[row, col] == 0) continue;
+                    //If a cell is a ship and it hasn't been scored
+                    //then do a simple return because we haven't finished the game
+                    if (hits[row, col] == false) return;
+                }
+            }
+
+            //If the loop succesfully completes then all
+            //ships have been destroyed and show the winLabel
+            winLabel.SetActive(true);
+            CancelInvoke("IncrementTime");
+        }
+
+        void IncrementTime()
+        {
+            //Add 1 to the time
+            time++;
+            //Update the time label with current time
+            //Format it mm:ss where m is the minute and s is the seconds
+            //ss should always display 2 digits
+            //mm should only display as many digits that are necessary
+            timeLabel.text = string.Format("{0}:{1}", time / 60, (time % 60).ToString("00"));
         }
     }
 }
